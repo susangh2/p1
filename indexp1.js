659c96f8c55f52c5ecea9ac4cb0c5e5a7b49e239
Susan <sanw1432@gmail.com>
2023-07-10 10:45:59 +0800

    change icon to flower and able to play song now

--- indexp1.js
+++ indexp1.js
@@ -1,166 +1,217 @@
 const unitLength = 20;
-const boxColor = 150;
+const boxColor = 144;
 const strokeColor = 50;
 let columns; /* To be determined by window width */
 let rows; /* To be determined by window height */
 let currentBoard;
 let nextBoard;
+let img;
+let song;
+
+
+//background//
+//change icon//
+//add sound//
+//not able to link the slider.value to the frameRate//
+
+function preload() {
+  backgroundImage = loadImage('flowers-g9e0519a31_1280.jpg');
+  song = loadSound("happy114950.mp3")
+  img = loadImage('redflower.png')
+}
+
+const slider = document.querySelector('#speed');
+const value = document.querySelector('.value');
+const speed = slider.value
+value.textContent = slider.value;
+slider.oninput = function () {
+  value.textContent = this.value;
+}
 
 
 function setup() {
-    /* Set the canvas to be under the element #canvas*/
-    const canvas = createCanvas(windowWidth, windowHeight - 100);
-    canvas.parent(document.querySelector("#canvas"));
-  
-    /*Calculate the number of columns and rows */
-    columns = floor(width / unitLength);
-    rows = floor(height / unitLength);
-  
-    /*Making both currentBoard and nextBoard 2-dimensional matrix that has (columns * rows) boxes. */
-    currentBoard = [];
-    nextBoard = [];
-    for (let i = 0; i < columns; i++) {
-      currentBoard[i] = [];
-      nextBoard[i] = [];
-    }
-    // Now both currentBoard and nextBoard are array of array of undefined values.
-    init(); // Set the initial values of the currentBoard and nextBoard
-    slider = createSlider(2, 20, 4, 1);
-    slider.position(213,665);
-  }
+  /* Set the canvas to be under the element #canvas*/
+  const canvas = createCanvas(windowWidth, windowHeight - 100);
+  canvas.parent(document.querySelector("#canvas"));
+  // background(backgroundImage);
 
-  /**
- * Initialize/reset the board state
- */
-function init() {
-    for (let i = 0; i < columns; i++) {
-      for (let j = 0; j < rows; j++) {
-        currentBoard[i][j] = 0;
-        nextBoard[i][j] = 0;
-      }
-    } 
+  /*Calculate the number of columns and rows */
+  columns = floor(width / unitLength);
+  rows = floor(height / unitLength);
+
+  /*Making both currentBoard and nextBoard 2-dimensional matrix that has (columns * rows) boxes. */
+  currentBoard = [];
+  nextBoard = [];
+  for (let i = 0; i < columns; i++) {
+    currentBoard[i] = [];
+    nextBoard[i] = [];
   }
+  // Now both currentBoard and nextBoard are array of array of undefined values.
+  init(); // Set the initial values of the currentBoard and nextBoard
+}
+
+/**
+* Initialize/reset the board state
+*/
+function init() {
+  for (let i = 0; i < columns; i++) {
+    for (let j = 0; j < rows; j++) {
+      currentBoard[i][j] = 0;
+      nextBoard[i][j] = 0;
+    }
+  } frameRate(slider.value);
+
+}
 
 function randomsetup() {
-    for (let i=0; i<columns; i++) {
-        for (let j=0; j<rows; j++){
-             currentBoard [i][j] = floor(random(2));
-        }
+  for (let i = 0; i < columns; i++) {
+    for (let j = 0; j < rows; j++) {
+      currentBoard[i][j] = floor(random(2));
     }
- }
-
-
-  function draw() {
-    background(255);
-    speedY = slider.value();
-    frameRate(speedY)
-    generate();
-    for (let i = 0; i < columns; i++) {
-      for (let j = 0; j < rows; j++) {
-        if (currentBoard[i][j] == 1) {
-          fill(boxColor);
-        } else {
-          fill(255);
-        }
-        stroke(strokeColor);
-        rect(i * unitLength, j * unitLength, unitLength, unitLength);
-      } 
-    } 
   }
+}
 
-  function generate() {
-    //Loop over every single box on the board
-    for (let x = 0; x < columns; x++) {
-      for (let y = 0; y < rows; y++) {
-        // Count all living members in the Moore neighborhood(8 boxes surrounding)
-        let neighbors = 0;
-        for (let i of [-1, 0, 1]) {
-          for (let j of [-1, 0, 1]) {
-            if (i == 0 && j == 0) {
-              // the cell itself is not its own neighbor
-              continue;
-            }
-            // The modulo operator is crucial for wrapping on the edge
-            neighbors +=
-              currentBoard[(x + i + columns) % columns][(y + j + rows) % rows];
+
+function draw() {
+  generate();
+  for (let i = 0; i < columns; i++) {
+    for (let j = 0; j < rows; j++) {
+
+      if (currentBoard[i][j] == 1) {
+        // image(img, this.i, this.j, width / unitLength, width / unitLength)
+        image(img, i * unitLength, j * unitLength, unitLength, unitLength, 0, 0)
+
+        // stroke(strokeColor);
+        // fill(boxColor);
+        // rect(i * unitLength, j * unitLength, unitLength, unitLength);
+
+      } else {
+        stroke(strokeColor);
+        fill(255);
+        rect(i * unitLength, j * unitLength, unitLength, unitLength);
+      }
+
+
+    }
+  }
+}
+
+function generate() {
+  //Loop over every single box on the board
+  for (let x = 0; x < columns; x++) {
+    for (let y = 0; y < rows; y++) {
+      // Count all living members in the Moore neighborhood(8 boxes surrounding)
+      let neighbors = 0;
+      for (let i of [-1, 0, 1]) {
+        for (let j of [-1, 0, 1]) {
+          if (i == 0 && j == 0) {
+            // the cell itself is not its own neighbor
+            continue;
           }
-        }
-  
-        // Rules of Life
-        if (currentBoard[x][y] == 1 && neighbors < 2) {
-          // Die of Loneliness
-          nextBoard[x][y] = 0;
-        } else if (currentBoard[x][y] == 1 && neighbors > 3) {
-          // Die of Overpopulation
-          nextBoard[x][y] = 0;
-        } else if (currentBoard[x][y] == 0 && neighbors == 3) {
-          // New life due to Reproduction
-          nextBoard[x][y] = 1;
-        } else {
-          // Stasis
-          nextBoard[x][y] = currentBoard[x][y];
+          // The modulo operator is crucial for wrapping on the edge
+          neighbors +=
+            currentBoard[(x + i + columns) % columns][(y + j + rows) % rows];
         }
       }
+
+      // Rules of Life
+      if (currentBoard[x][y] == 1 && neighbors < 2) {
+        // Die of Loneliness
+        nextBoard[x][y] = 0;
+      } else if (currentBoard[x][y] == 1 && neighbors > 3) {
+        // Die of Overpopulation
+        nextBoard[x][y] = 0;
+      } else if (currentBoard[x][y] == 0 && neighbors == 3) {
+        // New life due to Reproduction
+        nextBoard[x][y] = 1;
+      } else {
+        // Stasis
+        nextBoard[x][y] = currentBoard[x][y];
+      }
     }
-  
-    // Swap the nextBoard to be the current Board
-    [currentBoard, nextBoard] = [nextBoard, currentBoard];
   }
 
-  /**
- * When mouse is dragged
- */
+  // Swap the nextBoard to be the current Board
+  [currentBoard, nextBoard] = [nextBoard, currentBoard];
+}
+
+/**
+* When mouse is dragged
+*/
 function mouseDragged() {
-    /**
-     * If the mouse coordinate is outside the board
-     */
-    if (mouseX > unitLength * columns || mouseY > unitLength * rows) {
-      return;
-    }
-    const x = Math.floor(mouseX / unitLength);
-    const y = Math.floor(mouseY / unitLength);
-    currentBoard[x][y] = 1;
-    fill(boxColor);
-    stroke(strokeColor);
-    rect(x * unitLength, y * unitLength, unitLength, unitLength);
-  }
-  
   /**
-   * When mouse is pressed
+   * If the mouse coordinate is outside the board
    */
-  function mousePressed() {
-    noLoop();
-    mouseDragged();
+  if (mouseX > unitLength * columns || mouseY > unitLength * rows) {
+    return;
   }
-  
-  /**
-   * When mouse is released
-   */
-  function mouseReleased() {
-    loop();
+  const x = Math.floor(mouseX / unitLength);
+  const y = Math.floor(mouseY / unitLength);
+  currentBoard[x][y] = 1;
+  fill(boxColor);
+  stroke(strokeColor);
+  rect(x * unitLength, y * unitLength, unitLength, unitLength);
+}
+
+/**
+ * When mouse is pressed
+ */
+function mousePressed() {
+  noLoop();
+  mouseDragged();
+  if (!song.isPlaying()) {
+    song.play()
   }
+}
 
-  function pause() {
-    noLoop();
+/**
+ * When mouse is released
+ */
+function mouseReleased() {
+  loop();
+}
+
+function pause() {
+  noLoop();
+  song.stop();
+}
+
+
+function playgame() {
+  loop();
+}
+
+function keyPressed() {
+  switch (keyCode) {
+
+    case 80: // p for pause/play
+      running = !running;
+      break;
+
+    case 83: // s for save mode to save part as a photo
+      saveCanvas(canvas, 'my Canvas', "png");
+      break;
+
+
   }
+}
 
+document.querySelector("#reset-game").addEventListener("click", function () {
+  init();
+});
 
-  function playgame() {
-    loop();
-  }
+document.querySelector("#random-setup").addEventListener("click", function () {
+  randomsetup();
+});
 
-  document.querySelector("#reset-game").addEventListener("click", function () {
-    init();
-  });
+document.querySelector("#pausegame").addEventListener("click", function () {
+  pause();
+});
 
-  document.querySelector("#random-setup").addEventListener("click", function () {
-    randomsetup();
-  });
+document.querySelector("#playgame").addEventListener("click", function () {
+  playgame();
+});
 
-  document.querySelector("#pausegame").addEventListener("click", function () {
-    pause();
-  });
-
-  document.querySelector("#playgame").addEventListener("click", function () {
-    playgame();
-  });
+document.querySelector("#speed").addEventListener("input", function () {
+  slider;
+});
