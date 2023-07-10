6ec5fe52b5647156dfb229ec4835bd4eb1ae24bf
Susan <sanw1432@gmail.com>
2023-07-10 14:51:59 +0800

    2 columns and added Eventlistener

--- indexp1.js
+++ indexp1.js
@@ -1,6 +1,6 @@
 const unitLength = 20;
-const boxColor = 144;
-const strokeColor = 50;
+const boxColor = 255;
+const strokeColor = 255;
 let columns; /* To be determined by window width */
 let rows; /* To be determined by window height */
 let currentBoard;
@@ -8,16 +8,12 @@ let nextBoard;
 let img;
 let song;
 
-
-//background//
 //change icon//
-//add sound//
 //not able to link the slider.value to the frameRate//
 
 function preload() {
-  backgroundImage = loadImage('flowers-g9e0519a31_1280.jpg');
   song = loadSound("happy114950.mp3")
-  img = loadImage('redflower.png')
+  img = loadImage('yellowflower.png')
 }
 
 const slider = document.querySelector('#speed');
@@ -26,14 +22,14 @@ const speed = slider.value
 value.textContent = slider.value;
 slider.oninput = function () {
   value.textContent = this.value;
+  frameRate(parseInt(slider.value))
 }
 
 
 function setup() {
   /* Set the canvas to be under the element #canvas*/
-  const canvas = createCanvas(windowWidth, windowHeight - 100);
+  const canvas = createCanvas(windowWidth - 200, windowHeight - 200);
   canvas.parent(document.querySelector("#canvas"));
-  // background(backgroundImage);
 
   /*Calculate the number of columns and rows */
   columns = floor(width / unitLength);
@@ -59,8 +55,8 @@ function init() {
       currentBoard[i][j] = 0;
       nextBoard[i][j] = 0;
     }
-  } frameRate(slider.value);
-
+  }
+  frameRate(parseInt(slider.value));
 }
 
 function randomsetup() {
@@ -74,10 +70,15 @@ function randomsetup() {
 
 function draw() {
   generate();
+  // clear()
   for (let i = 0; i < columns; i++) {
     for (let j = 0; j < rows; j++) {
 
       if (currentBoard[i][j] == 1) {
+        stroke(strokeColor);
+        fill(234, 255, 193);
+        rect(i * unitLength, j * unitLength, unitLength, unitLength);
+        
         // image(img, this.i, this.j, width / unitLength, width / unitLength)
         image(img, i * unitLength, j * unitLength, unitLength, unitLength, 0, 0)
 
@@ -87,7 +88,7 @@ function draw() {
 
       } else {
         stroke(strokeColor);
-        fill(255);
+        fill(234, 255, 193);
         rect(i * unitLength, j * unitLength, unitLength, unitLength);
       }
 
@@ -148,7 +149,7 @@ function mouseDragged() {
   const x = Math.floor(mouseX / unitLength);
   const y = Math.floor(mouseY / unitLength);
   currentBoard[x][y] = 1;
-  fill(boxColor);
+  fill(205, 133, 63);
   stroke(strokeColor);
   rect(x * unitLength, y * unitLength, unitLength, unitLength);
 }
@@ -212,6 +213,13 @@ document.querySelector("#playgame").addEventListener("click", function () {
   playgame();
 });
 
-document.querySelector("#speed").addEventListener("input", function () {
-  slider;
-});
+// document.querySelector("#purpleflower").addEventListener("click", function () {
+//   img = loadImage('purpleflower.png')
+// });
+
+document.querySelectorAll(".flowercolor")
+  .forEach(button => {
+    button.addEventListener("click", function () {
+      img = loadImage(button.querySelector('img').src)
+    });
+  })
